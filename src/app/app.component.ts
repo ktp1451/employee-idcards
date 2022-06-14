26c991815732237fd22afcc93040f573743259c9
Katie Patterson <ktpatterson14@mgail.com>
2022-06-14 12:42:14 -0500

    updated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 
 @Component({
@@ -6,4 +6,32 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class CardFancyExample {}
+export class CardFancyExample implements OnInit { //always make sure to have the "implements OnInit" after the object name
+  //create an array of people
+  users=[
+    {
+      name: "Albus Dumbledore",
+      occupation: "Headmaster of Hogwarts",
+      age: 112,
+      hobbies: "Knitting"
+    },
+    {
+      name: "Harry Potter",
+      occupation: "Student at Hogwarts",
+      age: 16,
+      hobbies: "Quidditch"
+    },
+    {
+      name: "Arthur Weasley",
+      occupation: "MOM : Misuse of Muggle Artefacts Office",
+      age: 55,
+      hobbies: "Collector"
+    },
+  ]
+  //create a card for every person by using ngFor in HTML file 
+  //center card on page vertically and horizontally using flexbox in CSS file 
+  //BONUS implement ngIf in HTML file
+  ngOnInit(): void {
+    
+  }
+}
