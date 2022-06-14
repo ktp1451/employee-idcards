3f652447c1d771602a4fafc5e2dc72b73e4fd28b
Katie Patterson <ktpatterson14@mgail.com>
2022-06-14 10:16:48 -0500

    updated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component',
-  styleUrls: ['./app.component'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class CardFancyExample {}
